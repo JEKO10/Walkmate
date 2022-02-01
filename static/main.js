8053af9cc93b553b190fbfc6d859d3805f5f0fa6
Aleksa Bubanja <aleksa3b@gmail.com>
2022-02-01 23:55:07 +0100

    end section update

--- static/main.js
+++ static/main.js
@@ -3,7 +3,7 @@ const clouds = document.querySelector(".clouds");
 window.addEventListener("scroll", () => {
   const win = window.pageYOffset;
 
-  if (win > 2280) {
+  if (win > 1901) {
     clouds.style.height = 146 - +win / 50 + "%";
   }
 });
