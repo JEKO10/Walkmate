66ae6b4e1e57db8a729847fa2af3ee52487cbf23
Aleksa Bubanja <aleksa3b@gmail.com>
2022-02-02 18:19:59 +0100

    register page setup

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from logging import PlaceHolder
 from flask import Flask, render_template
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
@@ -17,6 +18,19 @@ class LoginForm(FlaskForm):
     remember = BooleanField("remember")
 
 
+class RegisterForm(FlaskForm):
+    email = StringField("email", validators=[InputRequired(), Email(
+        message="Invalid email"), Length(max=50)])
+    username = StringField("username", validators=[
+                           InputRequired(), Length(min=4, max=15)])
+    password = PasswordField("password", validators=[
+                             InputRequired(), Length(min=8, max=80)])
+    facebook = StringField("facebook", validators=[
+                           InputRequired(), Length(max=50)])
+    location = StringField("location", validators=[
+                           InputRequired(), Length(max=50)])
+
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -27,6 +41,13 @@ def login():
     form = LoginForm()
 
     return render_template("login.html", form=form)
+
+
+@app.route("/register")
+def register():
+    form = RegisterForm()
+
+    return render_template("register.html", form=form)
 
 
 if (__name__) == "__main__":
