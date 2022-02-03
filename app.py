d92a2c9b4ba60e2645000e0212575e8595708f48
Aleksa Bubanja <aleksa3b@gmail.com>
2022-02-03 22:15:27 +0100

    storing data in database and login logic finished

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from flask_bootstrap import Bootstrap
 from wtforms import StringField, BooleanField, PasswordField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "Thisissecreykeywhichissecret"
@@ -18,6 +19,7 @@ class User(db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+    facebook = db.Column(db.String(50))
     location = db.Column(db.String(50))
 
 
@@ -51,6 +53,14 @@ def index():
 def login():
     form = LoginForm()
 
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if user.password == form.password.data:
+                return redirect(url_for('dashboard'))
+
+        return '<h1>Invalid username or password</h1>'
+
     return render_template("login.html", form=form)
 
 
@@ -58,7 +68,18 @@ def login():
 def register():
     form = RegisterForm()
 
+    if form.validate_on_submit():
+        new_user = User(username=form.username.data, email=form.email.data,
+                        location=form.location.data, password=form.password.data, facebook=form.facebook.data)
+        db.session.add(new_user)
+        db.session.commit()
+
     return render_template("register.html", form=form)
+
+
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
 
 
 if (__name__) == "__main__":
