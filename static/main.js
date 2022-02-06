39a5a0e4b636ef45a42b0fd4255078740de31b9b
Aleksa Bubanja <aleksa3b@gmail.com>
2022-02-06 19:51:32 +0100

    sidebar finished

--- static/main.js
+++ static/main.js
@@ -7,3 +7,23 @@ window.addEventListener("scroll", () => {
     clouds.style.height = 146 - +win / 50 + "%";
   }
 });
+
+const sidebar = document.querySelector(".sidebar");
+const toggle = document.querySelector(".toggle");
+const toggleDiv = document.querySelector(".toggle div");
+const body = document.body;
+let showMenu = false;
+
+toggle.addEventListener("click", () => {
+  if (!showMenu) {
+    sidebar.classList.add("open");
+    toggleDiv.classList.add("open");
+    body.classList.add("fix");
+  } else {
+    sidebar.classList.remove("open");
+    toggleDiv.classList.remove("open");
+    body.classList.remove("fix");
+  }
+
+  showMenu = !showMenu;
+});
